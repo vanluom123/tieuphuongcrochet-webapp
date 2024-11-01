ba7bb450df58104b9140cc63815a80ac7b6acd36
vanluom123 <phanvanluom97bd@gmail.com>
2024-11-01 14:07:25 +0700

    add env var for google adsense

--- app/components/GoogleTag.tsx
+++ app/components/GoogleTag.tsx
@@ -15,7 +15,7 @@ export default function GoogleTag() {
 			`,
                 }}
             />
-            <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9595945500562321"
+            <Script async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_AD_CLIENT_ID}`}
                 crossOrigin="anonymous" />
         </>)
 }
