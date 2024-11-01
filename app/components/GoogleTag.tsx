f1098ea8f94013e5ebe01d89ee3efc8142279d35
Charles Phan <phanvanluom97bd@gmail.com>
2024-11-01 14:10:06 +0700

    Feature/setup google gsc ga adsend (#44)
    * add env variable for google-analytics
    
    * add env var for google adsense

--- app/components/GoogleTag.tsx
+++ app/components/GoogleTag.tsx
@@ -3,21 +3,19 @@ import Script from 'next/script'
 export default function GoogleTag() {
     return (
         <>
-        
-            <Script async strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-RTLCR6N9MQ"/>
+            <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`} />
             <Script
                 id="google-analytics"
-                strategy="afterInteractive"
                 dangerouslySetInnerHTML={{
                     __html: `
 				window.dataLayer = window.dataLayer || [];
 				function gtag(){dataLayer.push(arguments);}	
 				gtag('js', new Date());	
-				gtag('config', 'G-RTLCR6N9MQ');
+				gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
 			`,
                 }}
             />
-            <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9595945500562321"
+            <Script async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_AD_CLIENT_ID}`}
                 crossOrigin="anonymous" />
         </>)
 }
