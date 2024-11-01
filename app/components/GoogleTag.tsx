634d68eff62abcb23151fb639a28509e46fb8bfc
vanluom123 <phanvanluom97bd@gmail.com>
2024-11-01 13:59:56 +0700

    add env variable for google-analytics

--- app/components/GoogleTag.tsx
+++ app/components/GoogleTag.tsx
@@ -3,17 +3,15 @@ import Script from 'next/script'
 export default function GoogleTag() {
     return (
         <>
-        
-            <Script async strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-RTLCR6N9MQ"/>
+            <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`} />
             <Script
                 id="google-analytics"
-                strategy="afterInteractive"
                 dangerouslySetInnerHTML={{
                     __html: `
 				window.dataLayer = window.dataLayer || [];
 				function gtag(){dataLayer.push(arguments);}	
 				gtag('js', new Date());	
-				gtag('config', 'G-RTLCR6N9MQ');
+				gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
 			`,
                 }}
             />
