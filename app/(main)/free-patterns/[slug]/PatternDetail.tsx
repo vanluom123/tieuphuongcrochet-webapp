b044ac634fc00ff57d45cf1a40a6b25c1d44eb76
vanluom123 <phanvanluom97bd@gmail.com>
2024-11-03 22:41:49 +0700

    Lazy load ViewImagesList component to fix error 500

--- app/(main)/free-patterns/[slug]/PatternDetail.tsx
+++ app/(main)/free-patterns/[slug]/PatternDetail.tsx
@@ -3,13 +3,19 @@ import { Divider } from "antd";
 import { useTranslations } from "next-intl";
 import IntroductionCard from "@/app/components/introduction-card";
 import ViewDetailWrapper from "@/app/components/view-detail-wrapper";
-import ViewImagesList from "@/app/components/view-image-list";
 import { Pattern } from "@/app/lib/definitions";
- 
-const PatternDetail = ({pattern}: {pattern: Pattern}) => {
+import dynamic from "next/dynamic";
+
+// Lazy load ViewImagesList component
+const ViewImagesList = dynamic(
+    () => import("@/app/components/view-image-list"),
+    { ssr: false } // Disable server-side rendering
+);
+
+const PatternDetail = ({ pattern }: { pattern: Pattern }) => {
 
     const t = useTranslations("FreePattern");
-    
+
     return (
         <ViewDetailWrapper
             isShowAlert
@@ -19,6 +25,8 @@ const PatternDetail = ({pattern}: {pattern: Pattern}) => {
             {/* Introducing the free pattern */}
             <IntroductionCard isPreviewAvatar={false} data={pattern} isShowThumbnail />
             <Divider />
+
+            {/* ViewImagesList sẽ chỉ được render ở client side */}
             <ViewImagesList
                 isPattern
                 name='free-patterns'
