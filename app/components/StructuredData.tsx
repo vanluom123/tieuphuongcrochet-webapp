981a3d414000a2b8747449b807585b09c5a60bd9
vanluom123 <phanvanluom97bd@gmail.com>
2024-11-06 15:03:51 +0700

    add offers property for StucData product

--- app/components/StructuredData.tsx
+++ app/components/StructuredData.tsx
@@ -5,15 +5,33 @@ interface StructuredDataProps {
   description: string;
   url: string;
   type?: string;
+  offers?: {
+    price: number;
+    priceCurrency: string;
+    availability: string;
+    url: string;
+  };
+  review?: {
+    reviewRating: {
+      ratingValue: number;
+      bestRating: number;
+    };
+  };
+  aggregateRating?: {
+    ratingValue: number;
+    reviewCount: number;
+  };
 }
 
-const StructuredData: React.FC<StructuredDataProps> = ({ title, description, url, type = "WebSite" }) => {
+const StructuredData: React.FC<StructuredDataProps> = ({ title, description, url, type = "WebSite", offers, ...restProps }) => {
   const structuredData = {
     "@context": "https://schema.org",
     "@type": type,
     "name": title,
     "description": description,
     "url": url,
+    "offers": offers,
+    ...restProps
   };
 
   return (
