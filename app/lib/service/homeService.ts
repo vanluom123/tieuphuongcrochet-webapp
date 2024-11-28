6cc17d76cb52d0b761e03b5fbb953fa983f0fd79
Charles Phan <phanvanluom97bd@gmail.com>
2024-11-28 15:41:53 +0700

    fix show image in home (#66)
    * fix show image in home
    
    * remove unused code

--- app/lib/service/homeService.ts
+++ app/lib/service/homeService.ts
@@ -1,7 +1,7 @@
 import { map } from "lodash";
 import { API_ROUTES } from "../constant";
 import { Product, Pattern, Post, HomeData } from "../definitions";
-import { getAvatar, mapImagesPreview } from "../utils";
+import { getAvatar } from "../utils";
 import apiService from "./apiService";
 
 export const fetchHomeData = async (): Promise<HomeData> => {
@@ -22,8 +22,7 @@ export const fetchHomeData = async (): Promise<HomeData> => {
         id: pt.id,
         author: pt.author,
         name: pt.name,
-        src: pt.images?.[0]?.fileContent,
-        imagesPreview: mapImagesPreview(pt.images || [])
+        src: pt.fileContent || pt.images?.[0]?.fileContent,
     }));
 
     const products: Product[] = map(data.products, prod => ({
@@ -31,14 +30,13 @@ export const fetchHomeData = async (): Promise<HomeData> => {
         name: prod.name,
         price: prod.price,
         currency_code: prod.currency_code,
-        src: prod.images?.[0]?.fileContent,
-        imagesPreview: mapImagesPreview(prod.images || []),
+        src: prod.fileContent || prod.images?.[0]?.fileContent,
         category: prod.category
     }));
 
     const blogs: Post[] = map(data.blogs, bl => ({
         ...bl,
-        src: getAvatar(bl.files || [])
+        src: bl.fileContent || getAvatar(bl.files || [])
     }));
     return {
         freePatterns,
