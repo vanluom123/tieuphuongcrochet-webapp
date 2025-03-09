933dbd1a263d57fb17359b9608be21fa1919ade8
Phan Văn Lượm <phanvanluom97bd@gmail.com>
2025-03-09 23:26:39 +0700

    Add Message Notification

--- app/lib/service/settingService.ts
+++ app/lib/service/settingService.ts
@@ -4,73 +4,64 @@ import { Banner, DataType, IBannerType } from "../definitions";
 import { notification } from "../notify";
 import apiJwtService from "./apiJwtService";
 
-const handleApiError = (err: any) => {
-    console.error("API Error:", err);
-    notification.error({ message: 'Failed', description: err.message });
-    return []; // Re-throw the error to be caught by the caller
-};
-
 export const fetchBannerTypes = async (): Promise<DataType[]> => {
-    try {
-        const res = await apiJwtService({ endpoint: API_ROUTES.BANNER_TYPES });
-        console.log("Banner Types:", res.data);
-        return map(res.data, ({ id, name, createdDate }) => ({
-            id,
-            name,
-            key: id || '',
-            createdDate
-        }));
-    } catch (err) {
-        return handleApiError(err);
-    }
+    const res = await apiJwtService({ endpoint: API_ROUTES.BANNER_TYPES });
+    return map(res.data, ({ id, name, createdDate }) => ({
+        id,
+        name,
+        key: id || '',
+        createdDate
+    }));
 };
 
 export const fetchBanners = async (): Promise<Banner[]> => {
-    try {
-        const res = await apiJwtService({ endpoint: API_ROUTES.BANNERS });
-        console.log("Banners:", res.data);
-        return map(res.data, d => ({
-            ...d,
-            bannerTypeId: d.bannerType?.id
-        }));
-    } catch (err) {
-        return handleApiError(err);
-    }
+    const res = await apiJwtService({ endpoint: API_ROUTES.BANNERS });
+    return map(res.data, d => ({
+        ...d,
+        bannerTypeId: d.bannerType?.id
+    }));
 };
 
 export const createUpdateBanners = async (banners: Banner[]): Promise<void> => {
-    try {
-        await apiJwtService({
-            endpoint: API_ROUTES.BANNERS,
-            method: 'POST',
-            data: banners
-        });
-    } catch (err) {
-        handleApiError(err);
+    const res = await apiJwtService({
+        endpoint: API_ROUTES.BANNERS,
+        method: 'POST',
+        data: banners
+    });
+    if (res.success) {
+        notification.success({
+            message: 'Success',
+            description: 'Banners updated successfully'
+        })
+    } else {
+        notification.error({
+            message: 'Failed',
+            description: 'An error occurred while updating the banner'
+        })
     }
 };
 
 export const deleteBannerType = async (id: string): Promise<void> => {
-    try {
-        await apiJwtService({
-            endpoint: `${API_ROUTES.BANNER_TYPES}/${id}`,
-            method: 'DELETE'
-        });
+    const res = await apiJwtService({
+        endpoint: `${API_ROUTES.BANNER_TYPES}/${id}`,
+        method: 'DELETE'
+    });
+    if (res.success) {
         notification.success({ message: 'Success', description: 'Delete banner type successfully' });
-    } catch (err) {
-        handleApiError(err);
+    } else {
+        notification.error({ message: 'Failed', description: 'An error occurred while deleting the banner type' });
     }
 };
 
 export const createUpdateBannerType = async (data: IBannerType): Promise<void> => {
-    try {
-        await apiJwtService({
-            endpoint: API_ROUTES.BANNER_TYPES,
-            method: 'POST',
-            data
-        });
+    const res = await apiJwtService({
+        endpoint: API_ROUTES.BANNER_TYPES,
+        method: 'POST',
+        data
+    });
+    if (res.success) {
         notification.success({ message: 'Success', description: 'Create banner type successfully' });
-    } catch (err) {
-        handleApiError(err);
+    } else {
+        notification.error({ message: 'Failed', description: 'An error occurred while creating the banner type' });
     }
 };
