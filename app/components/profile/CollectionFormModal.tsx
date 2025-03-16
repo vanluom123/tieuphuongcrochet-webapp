3419ea958168c502015f5a7157895a1e1e7b7fac
Phan Văn Lượm <phanvanluom97bd@gmail.com>
2025-03-16 12:46:37 +0700

    Cải thiện định dạng mã nguồn trong CollectionFormModal, bao gồm việc thêm khoảng trắng và cải thiện cách xử lý trạng thái tải. Cập nhật các hàm để đảm bảo tính nhất quán và dễ đọc hơn.

--- app/components/profile/CollectionFormModal.tsx
+++ app/components/profile/CollectionFormModal.tsx
@@ -1,11 +1,11 @@
-import {Form, Input, Modal} from 'antd';
-import {useTranslations} from 'next-intl';
-import {Collection} from '@/app/lib/definitions';
-import {useEffect, useState} from 'react';
-import {createCollection, fetchCollectionDetail, updateCollection} from '@/app/lib/service/profileService';
-import {notification} from '@/app/lib/notify';
+import { Form, Input, Modal } from 'antd';
+import { useTranslations } from 'next-intl';
+import { Collection } from '@/app/lib/definitions';
+import { useEffect, useState } from 'react';
+import { createCollection, fetchCollectionDetail, updateCollection } from '@/app/lib/service/profileService';
+import { notification } from '@/app/lib/notify';
 
-const {Item} = Form;
+const { Item } = Form;
 
 interface CollectionFormModalProps {
     modalData: {
@@ -21,7 +21,7 @@ const initialState = {
     collection: {} as Collection
 }
 
-const CollectionFormModal = ({modalData, setModalData, onRefreshData}: CollectionFormModalProps) => {
+const CollectionFormModal = ({ modalData, setModalData, onRefreshData }: CollectionFormModalProps) => {
     const [form] = Form.useForm();
     const t = useTranslations('Profile');
     const [state, setState] = useState(initialState);
@@ -35,17 +35,18 @@ const CollectionFormModal = ({modalData, setModalData, onRefreshData}: Collectio
 
     useEffect(() => {
         if (modalData.id && modalData.open) {
+            setState(prev => ({ ...prev, loading: true }));
             fetchCollectionDetail(modalData.id)
                 .then(collection => {
                     form.setFieldsValue(collection);
-                    setState({loading: false, collection});
-                });
+                    setState(prev => ({ ...prev, collection: collection }));
+                }).finally(() => setState(prev => ({ ...prev, loading: false })));
         }
     }, [modalData.id, modalData.open, form]);
 
     const onHandleCancel = () => {
         form.resetFields();
-        setModalData({open: false, id: ''});
+        setModalData({ open: false, id: '' });
     }
 
     const onHandleOk = () => {
@@ -53,13 +54,13 @@ const CollectionFormModal = ({modalData, setModalData, onRefreshData}: Collectio
     }
 
     const onSubmitForm = async (name: string) => {
-        setState(prev => ({...prev, loading: true}));
-        const res = modalData.id 
-            ? await updateCollection(modalData.id, name) 
+        setState(prev => ({ ...prev, loading: true }));
+        const res = modalData.id
+            ? await updateCollection(modalData.id, name)
             : await createCollection(name);
 
         form.resetFields();
-        setModalData({open: false, id: ''});
+        setModalData({ open: false, id: '' });
         notification[res.success ? 'success' : 'error']({
             message: res.success ? 'Success' : 'Failed',
             description: res.message
@@ -69,7 +70,7 @@ const CollectionFormModal = ({modalData, setModalData, onRefreshData}: Collectio
             onRefreshData();
         }
 
-        setState(prev => ({...prev, loading: false}));
+        setState(prev => ({ ...prev, loading: false }));
     }
 
     return (
@@ -82,12 +83,12 @@ const CollectionFormModal = ({modalData, setModalData, onRefreshData}: Collectio
             destroyOnClose={true}
             confirmLoading={state.loading}
         >
-            <Form form={form} onFinish={({name}) => onSubmitForm(name)}>
+            <Form form={form} onFinish={({ name }) => onSubmitForm(name)}>
                 <Item
                     name="name"
-                    rules={[{required: true, message: 'Please input collection name!'}]}
+                    rules={[{ required: true, message: 'Please input collection name!' }]}
                 >
-                    <Input placeholder={t('collections.name')}/>
+                    <Input placeholder={t('collections.name')} />
                 </Item>
             </Form>
         </Modal>
