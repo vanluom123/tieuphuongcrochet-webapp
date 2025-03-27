8c2970811f75c17b48c1ca2a178463b3b7d91023
minhvy350 <minhvy350@outlook.com>
2025-03-27 22:40:32 +0700

    Wrap FreePatternsNode component with BookmarkModalProvider for context management

--- app/components/home/FreePatternsNode.tsx
+++ app/components/home/FreePatternsNode.tsx
@@ -10,6 +10,7 @@ import { ROUTE_PATH } from "@/app/lib/constant";
 import ReadMoreBtn from "../read-more";
 import { Pattern } from "@/app/lib/definitions";
 import { useRouter } from "next/navigation";
+import { BookmarkModalProvider } from "@/app/context/BookmarkModalContext";
 
 const FreePatternsNode = ({patterns}: {patterns: Pattern[]}) => {
 
@@ -19,34 +20,36 @@ const FreePatternsNode = ({patterns}: {patterns: Pattern[]}) => {
 	};
 	
 	return (
-		<div className='patterns'>
-			<HeaderPart
-				titleId='Home.FreePattern.title'
-				descriptionId='Home.FreePattern.description'
-				isShowDivider
-			/>
-			<Row gutter={[{ xs: 8, sm: 16, xl: 24 }, { xs: 12, sm: 16, xl: 24 }]}>
-				{
-					map(patterns, (pattern, index) =>
-						<Col key={`freepattern_${index}`} xs={12} sm={8} lg={6} >
-							<FreePatternCard
-								pattern={pattern}
-								onReadDetail={() => onViewPattern(pattern.id || '')}
-							/>
-						</Col>
-					)
-				}
-			</Row>
-			{patterns?.length > 0 ?
-				< div className='read-more'>
-					<ReadMoreBtn path={ROUTE_PATH.FREEPATTERNS} />
-				</div> :
-				<Empty
-					imageStyle={{ height: 80 }}
-					image={Empty.PRESENTED_IMAGE_SIMPLE}
+		<BookmarkModalProvider>
+			<div className='patterns'>
+				<HeaderPart
+					titleId='Home.FreePattern.title'
+					descriptionId='Home.FreePattern.description'
+					isShowDivider
 				/>
-			}
-		</div >
+				<Row gutter={[{ xs: 8, sm: 16, xl: 24 }, { xs: 12, sm: 16, xl: 24 }]}>
+					{
+						map(patterns, (pattern, index) =>
+							<Col key={`freepattern_${index}`} xs={12} sm={8} lg={6} >
+								<FreePatternCard
+									pattern={pattern}
+									onReadDetail={() => onViewPattern(pattern.id || '')}
+								/>
+							</Col>
+						)
+					}
+				</Row>
+				{patterns?.length > 0 ?
+					< div className='read-more'>
+						<ReadMoreBtn path={ROUTE_PATH.FREEPATTERNS} />
+					</div> :
+					<Empty
+						imageStyle={{ height: 80 }}
+						image={Empty.PRESENTED_IMAGE_SIMPLE}
+					/>
+				}
+			</div >
+		</BookmarkModalProvider>
 	)
 }
 
